2a79a4456a2d3dd9d6f9cde34ce5f394c2397d1e
defrxst <growtopiastfu123@proton.me>
2023-09-14 13:19:35 +0000

    initial 8 ball commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import EightBall from './lib/8Ball.svelte'
+</script>
+
+<main class="mx-auto bg-white h-screen">
+  <h1 class="text-6xl text-black absolute left-1/2 -translate-x-1/2">Magic 8 Ball</h1>
+  <EightBall/>
+</main>
---
+++ src/lib/8Ball.svelte
@@ -0,0 +1,41 @@
+<script>
+
+  let ball
+
+  let replies = [ // replies
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definetly",
+    "You may rely on it",
+    "As i see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ]
+
+  function roll() {
+    let randomNumber = Math.floor(Math.random() * replies.length)
+    ball.style.fontSize = '20px'
+    ball.innerHTML = replies[randomNumber]
+  }
+
+</script>
+
+<div on:click={()=>roll()} class="w-96 h-96 bg-black left-1/2 -translate-x-1/2 rounded-full m-auto text-center mt-48 absolute flex justify-center items-center">
+  <div class="w-64 h-64 bg-white rounded-full flex justify-center items-center">
+    <h1 bind:this={ball} class="text-[130px] text-black">8</h1>
+  </div>
+</div>
+
